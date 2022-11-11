f0f3bca67a476e24b6a73fb65df01e6c80a58041
Razkoo <merdok226@gmail.com>
2022-11-11 21:12:42 +0200

    shitty movement and shitty floor

--- Assets/scripts/camMove.cs
+++ Assets/scripts/camMove.cs
@@ -6,7 +6,7 @@ public class camMove : MonoBehaviour
 {
     private Vector2 turn;
     private float xm = 0, ym = 0, zm = 0;
-    bool a, d, w, s, space;
+    bool r, l, u, d, o, p;
 
     void Start()
     {
@@ -15,15 +15,22 @@ public class camMove : MonoBehaviour
 
     void Update()
     {
+        r = Input.GetKey(KeyCode.RightArrow);
+        l = Input.GetKey(KeyCode.LeftArrow);
+        u = Input.GetKey(KeyCode.UpArrow);
+        d = Input.GetKey(KeyCode.DownArrow);
+        o = Input.GetKey(KeyCode.O);
+        p = Input.GetKey(KeyCode.P);
+
         turn.x += Input.GetAxis("Mouse X"); 
         turn.y += Input.GetAxis("Mouse Y");
 
-        if (Input.GetKeyDown(KeyCode.RightArrow)) xm = 0.3f;
-        else if (Input.GetKeyDown(KeyCode.LeftArrow)) xm = -0.3f;
-        if (Input.GetKeyDown(KeyCode.UpArrow)) zm = 0.4f;
-        else if (Input.GetKeyDown(KeyCode.DownArrow)) zm = -0.4f;
-        if (Input.GetKeyDown(KeyCode.O)) ym = 0.4f;
-        else if (Input.GetKeyDown(KeyCode.P)) ym = -0.4f;
+        if (r) xm = 0.3f;
+        else if (l) xm = -0.3f;
+        if (u) zm = 0.4f;
+        else if (d) zm = -0.4f;
+        if (o) ym = 0.4f;
+        else if (p) ym = -0.4f;
 
         transform.eulerAngles = new Vector3(-turn.y, turn.x, 0.0f); // Mouse angle
         transform.position = new Vector3(transform.position.x + xm, transform.position.y + ym, transform.position.z +  zm); // Player angle
